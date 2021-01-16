cf3001c75f380a47b757d2af02e2f1d5faa68726
Mariusz Ilijew <mariu6@gmail.com>
2021-01-16 12:19:25 +0100

    Updates

---
+++ precache-manifest.b87fc7bb8e47581a46e48c6316905d2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b3934ed764dcf82787524a11b6c0906",
+    "url": "/React-tic-tac-toe-workshop/index.html"
+  },
+  {
+    "revision": "f1c8dd9446926ac332cd",
+    "url": "/React-tic-tac-toe-workshop/static/css/main.619f82af.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/React-tic-tac-toe-workshop/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-tic-tac-toe-workshop/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1c8dd9446926ac332cd",
+    "url": "/React-tic-tac-toe-workshop/static/js/main.832b21d3.chunk.js"
+  },
+  {
+    "revision": "8c170f480dedb8a47df7",
+    "url": "/React-tic-tac-toe-workshop/static/js/runtime-main.be58eca4.js"
+  }
+]);
